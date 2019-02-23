7cb1559db8992b42e74c13bdc24a1cbd8a980831
yourshansel <chenhan.sg@gmail.com>
2019-02-23 12:37:30 +0800

    test

--- src/BodyComponent.js
+++ src/BodyComponent.js
@@ -29,7 +29,7 @@ class Body extends Component{
     return(
       <Wrapper>
       <BodyText>
-          A digital product designer from Singapore running <Link href="https://neede.co" target="_blank" rel="noreferrer">Neede </Link>
+          Test A digital product designer from Singapore running <Link href="https://neede.co" target="_blank" rel="noreferrer">Neede </Link>
           as a side project. Final year student at the <Link href="http://nus.edu.sg/" target="_blank" rel="noreferrer">National University of Singapore</Link>.
           Former Design Intern at <Link href="https://aaptiv.com/" target="_blank" rel="noreferrer">Aaptiv</Link> and <Link href="https://www.codigo.co/work" target="_blank" rel="noreferrer">Codigo</Link>.
       </BodyText>
